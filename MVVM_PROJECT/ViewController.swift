d3d50813ef16919234f88279d9cf073631024168
Andre Linces <andrelinces@yahoo.com.br>
2022-03-18 11:23:50 -0300

    Initial Commit

---
+++ MVVM_PROJECT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_PROJECT
+//
+//  Created by Andre Linces on 18/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
